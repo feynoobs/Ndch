e4d491d510643571dfbe1ea9ec4ccdeb015536b7
feynoobs <feynoobs@gmail.com>
2024-07-29 19:40:49 +0900

    タブ幅

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(const MyApp());
+    runApp(const Ndch());
 }
 
-class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+class Ndch extends StatelessWidget {
+    const Ndch({super.key});
 
     // This widget is the root of your application.
     @override
@@ -31,13 +31,13 @@ class MyApp extends StatelessWidget {
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
             ),
-            home: const MyHomePage(title: 'Flutter Demo Home Page'),
+            home: const NdchHome(title: 'Flutter Demo Home Page'),
         );
     }
 }
 
-class MyHomePage extends StatefulWidget {
-    const MyHomePage({super.key, required this.title});
+class NdchHome extends StatefulWidget {
+    const NdchHome({super.key, required this.title});
 
     // This widget is the home page of your application. It is stateful, meaning
     // that it has a State object (defined below) that contains fields that affect
@@ -51,10 +51,10 @@ class MyHomePage extends StatefulWidget {
     final String title;
 
     @override
-    State<MyHomePage> createState() => _MyHomePageState();
+    State<NdchHome> createState() => _NdchHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _NdchHomeState extends State<NdchHome> {
       int _counter = 0;
 
       void _incrementCounter() {
