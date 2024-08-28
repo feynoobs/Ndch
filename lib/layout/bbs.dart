b2573741d21e969989812bdd5f3c98680ab8ff72
feynoobs <p.w.feeniej@gmail.com>
2024-08-28 17:01:02 +0900

    乃木将軍

--- lib/layout/bbs.dart
+++ lib/layout/bbs.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../dao/board_object.dart';
 import '../database/database.dart';
 import '../database/get_bbs.dart';
+import 'common.dart';
 
 class BBS extends StatefulWidget
 {
@@ -14,26 +15,56 @@ class BBS extends StatefulWidget
 class _BBSState extends State<BBS>
 {
     List<BoardObject>? bbses;
-/*
+    List<Widget> aaa = [];
+
     Future<void> _initialize() async
     {
-        final ins = await DB.getInstance();
-        bbses = await GetBBS(ins).get();
+        bbses = await GetBBS(await DB.getInstance()).get();
+        setState(() {
+            int axa = 0;
+            for (final BoardObject bbs in bbses!) {
+                ++axa;
+                aaa!.add(Container
+                (
+                    key: ValueKey(axa),
+                    decoration: const BoxDecoration(
+                        border: Border(
+                            bottom: BorderSide(
+                                color: Colors.grey,
+                                width: 0.2
+                            ),
+                        ),
+                    ),
+                    child: Text(bbs.group)
+                ));
+            }
+        });
     }
 
     @override
-    void initState() async
+    void initState()
     {
         super.initState();
         _initialize();
     }
-*/
 
     @override
     Widget build(BuildContext context)
     {
-        return Row(
+        final ScrollController scrollController = ScrollController();
 
-        );
+        return Scaffold(
+            appBar: const EmptyAppBar(),
+            body: NotificationListener<ScrollNotification> (
+                child: Scrollbar(child: ListView.builder(
+                    controller: scrollController,
+                    shrinkWrap: true,
+                    itemCount: bbses!.length,
+                    itemBuilder: (final BuildContext _, final int index) {
+                        return aaa![index];
+                    },
+                ),
+            )
+        ));
     }
 }
